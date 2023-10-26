6b0ac19a0469dc4255b6422377a4ac9625dbafde
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-25 22:43:34 -0300

    cambio ruta base

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])
+Route::get('/', [\App\Http\Controllers\SessionController::class, 'loginForm'])
   ->name('inicio');
