1e73115130e4d17c6112e7b7a12d65e9e1a607fe
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-10-25 22:43:14 -0300

    migrations

---
+++ database/migrations/2023_10_25_232653_create_places_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('coordinates');
+            $table->text('description')->nullable();
+            $table->boolean('access_entrance')->default(false);
+            $table->boolean('assisted_access_entrance')->default(false);
+            $table->boolean('internal_circulation')->default(false);
+            $table->boolean('bathroom')->default(false);
+            $table->boolean('adult_changing_table')->default(false);
+            $table->boolean('parking')->default(false);
+            $table->boolean('elevator')->default(false);
+            $table->unsignedInteger('src_info_id'); // FK
+            $table->unsignedInteger('review_id'); // FK
+            $table->unsignedInteger('category_id'); // FK
+            $table->unsignedInteger('uploaded_from_id'); // FK
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('places');
+    }
+};
