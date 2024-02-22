b8e3e7eb4cc51b1f4001a25b97bce433cf5e0999
Pedro Gonzalez <pedro.gch01@gmail.com>
2024-02-22 01:53:55 -0300

    arreglos de estilos

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
-@section('title', 'Blog')
+@section('title', 'Blog de nivelo, noticias de accesibilidad')
 
 
 @section('header')
@@ -15,36 +15,38 @@
   <div class="row d-flex vh-100">
     <div class="mb-2">
       <div class="row my-4 mx-auto">
-        <div class="col-12 my-4 border-bottom border-dark-subtle pb-3">
+        <div class="col-12 my-2 border-bottom border-dark-subtle pb-3">
           <div class="d-flex ">
             <h2 class="h3 fw-bold">Blog</h2>
             <span class="bg-movimiento ms-3"></span>
           </div>
         </div>
 
-        <div class="row d-flex col-12 my-4">
-          {{-- acá arranca el foreach --}}
+        <div class="row d-flex col-12 my-2">
+
+          @foreach ($noticias as $noticia)
           <article class="col-4">
             <div class="my-2">
-              <h3>Maternindad y accesibilidad</h3>
+              <h3>{{ $noticia->title }}</h3>
             </div>
             <div class="imagenDeTapa">
               <img src="{{asset('storage/places/kSysaXa4dXROO7kTgdV6hZSEMskBJNZdb4SmpQIE.jpg') }}" class="d-block w-100 1:3" alt="una imagen">
             </div>
             <div class="my-2">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi illum asperiores velit ex incidunt reprehenderit modi dignissimos possimus molestiae inventore dicta doloremque similique necessitatibus minus distinctio, molestias dolorum repudiandae! Quos.</p>
+              <p>{{ $noticia->descripcion_reducida() }}</p>
             </div>
             <div class="my-2">
               <div>
-                <form action="#" method="get">
+                <form action="{{ url('/blog/' . $noticia->id . '/leer_mas') }}" method="get">
                   <button class="btn bg-verde-principal" type="submit">
                     Leer más
                   </button>
+                </form>
               </div>
             </div>
           </article>
+          @endforeach
 
-          {{-- acá cierra el foreach --}}
         </div>
       </div>
     </div>
