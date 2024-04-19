d90e1ab2171b246153ae8679699cd0d03ffa7672
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2024-04-19 01:19:57 -0300

    breadcrumbs en detalle de noticia

--- resources/views/blog/articuloCompleto.blade.php
+++ resources/views/blog/articuloCompleto.blade.php
@@ -1,3 +1,11 @@
+<?php
+/**
+ * @var \App\Models\Noticia $noticia
+ * @var \App\Models\Noticia[] $noticias
+ */
+
+?>
+
 @extends('layouts.main')
 
 @section('title', 'Blog :: '.$noticia->title)
@@ -17,15 +25,19 @@
       <div class="row my-4 mx-auto">
         <div class="col-12 my-2 border-bottom border-dark-subtle pb-3">
           <div class="d-flex ">
-            <h2 class="h3 fw-bold">Blog</h2>
-            <span class="bg-movimiento ms-3"></span>
+            <a href="{{ route('blogIndex') }}"><img src="{{ url('/img/icons/back_icon.svg') }}" alt="atrás" class=" me-1" height="20px"></a>
+            <a href="{{ route('blogIndex') }}" class="text-reset text-decoration-none">
+              <p class="h5 fw-bold">Blog / Noticia</p>
+            </a>
+
           </div>
         </div>
 
         <div class="row d-flex col-12 my-2">
           <article class="col-12">
-            <div class="my-3">
-              <h3>{{ $noticia->title }}</h3>
+            <div class="my-4">
+              <h2>{{ $noticia->title }}</h2>
+              <p> {{ $noticia->created_at }}</p>
             </div>
             <div class="imagenDeTapa mt-3 mb-5">
               <img src="{{asset('storage/' . $noticia->image)}}" class="d-block w-100" alt="{{$noticia->alt}}">
@@ -45,49 +57,29 @@
           </article>
         </div>
         <div class="container">
-          <div class="row">
-            <div class="col-12 newsContainer">
-              <div class="col-sm-4">
-                <div class="card">
-                  <img src="{{asset('storage/places/kSysaXa4dXROO7kTgdV6hZSEMskBJNZdb4SmpQIE.jpg') }}" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Título de la Noticia 1</h5>
+          <div class="d-flex">
+            <h2 class="h3 fw-bold">Últimas noticias</h2>
+            <span class="bg-movimiento ms-3"></span>
+          </div>
+          <div class="row my-3">
+            <div class="col-12 newsContainer d-flex justify-content-around">
+              @foreach($noticias as $noticia)
+              <a href="{{ url('/blog/' . $noticia->id . '/leer_mas') }}" class="text-decoration-none">
+                <div class="col-sm-4">
+                  <div class="card">
+                    <img src="{{asset('storage/' . $noticia->image ) }}" class="card-img-top" alt="{{ $noticia->alt }}">
+                    <div class="card-body">
+                      <h3 class="card-title"> {{ $noticia->title }}</h3>
+                      <p> {{ $noticia->created_at }}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="col-sm-4">
-                <div class="card">
-                  <img src="{{asset('storage/places/kSysaXa4dXROO7kTgdV6hZSEMskBJNZdb4SmpQIE.jpg') }}" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Título de la Noticia 1</h5>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-4">
-                <div class="card">
-                  <img src="{{asset('storage/places/kSysaXa4dXROO7kTgdV6hZSEMskBJNZdb4SmpQIE.jpg') }}" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Título de la Noticia 1</h5>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-4">
-                <div class="card">
-                  <img src="{{asset('storage/places/kSysaXa4dXROO7kTgdV6hZSEMskBJNZdb4SmpQIE.jpg') }}" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Título de la Noticia 1</h5>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-4">
-                <div class="card">
-                  <img src="{{asset('storage/places/kSysaXa4dXROO7kTgdV6hZSEMskBJNZdb4SmpQIE.jpg') }}" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Título de la Noticia 1</h5>
-                  </div>
-                </div>
-              </div>
+              </a>
+              @endforeach
             </div>
+          </div>
+          <div class="my-5">
+            <a href="{{ route('blogIndex') }}" class="form-control btn rounded-pill p-3 shadow-sm bg-verde-principal  btn-verde-hover text-white">Ver todas</a>
           </div>
         </div>
 
