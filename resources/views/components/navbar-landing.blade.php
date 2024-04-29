6fc3de731a4dc194f482bb303b675afa0834211f
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2024-04-28 22:56:25 -0300

    navbar landing

--- resources/views/components/navbar-landing.blade.php
+++ resources/views/components/navbar-landing.blade.php
@@ -1,3 +1,28 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+<div class="fixed-top">
+  <nav class="navbar bg-violeta-dark navbar-expand-lg">
+    <div class="container container-fluid py-2 d-flex align-items-center">
+      <div>
+        <a class="navbar-brand" href="{{ route('home') }}"><img src="{{ url('/img/logo_h_blanco.png') }}" alt="logo de nivelo"></a>
+      </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item  me-4">
+            <a class="nav-link active text-light" aria-current="page" href="#">HOME</a>
+          </li>
+          <li class="nav-item  me-4">
+            <a class="nav-link text-light" href="#">LA APP</a>
+          </li>
+          <li class="nav-item  me-4">
+            <a class="nav-link text-light" href="#">CONTACTO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled text-white-50" aria-disabled="true">DESCARGAR</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </div>
