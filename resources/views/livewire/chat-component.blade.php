7ec40e5fd6020633aee28516dae66ab25a0a7dcf
Pedro Gonzalez <pedro.gch01@gmail.com>
2024-05-06 16:08:52 -0300

    a basic view chat with livewire

---
+++ resources/views/livewire/chat-component.blade.php
@@ -0,0 +1,7 @@
+<div>
+    {{-- Because she competes with no one, no one can compete with her. --}}
+    <form wire:submit="submitMessage">
+      <input wire:model="message" />
+      <button type="submit">send</button>
+    </form>
+</div>
