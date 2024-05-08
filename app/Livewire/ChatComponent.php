7d0b28e71df167be210d1392af31adc0ad32f535
Pedro Gonzalez <pedro.gch01@gmail.com>
2024-05-07 21:29:28 -0300

    logic chat

--- app/Livewire/ChatComponent.php
+++ app/Livewire/ChatComponent.php
@@ -3,16 +3,42 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\On;
+use App\Events\MessageEvent;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Message;
 
 class ChatComponent extends Component
 {
   public $message;
-  public $messages = [];
+  public $conversation = [];
 
+  public function mount()
+  {
+
+    $messages = Message::all();
+
+    foreach ($messages as $m)
+    {
+      $this->conversation[] = [
+        'username' => $m->user->name,
+        'message'  => $m->message
+      ];
+    }
+  }
   public function submitMessage()
   {
-    dump($this->message);
+    MessageEvent::dispatch(Auth::user()->id, $this->message);
     $this->message = "";
+  }
+  #[On('echo:chat-channel,MessageEvent')]
+  public function listenForMessage($data)
+  {
+    dd($data);
+    $this->conversation[] = [
+      'username' => $data->username,
+      'message'  => $data['message']
+    ];
   }
   public function render()
   {
