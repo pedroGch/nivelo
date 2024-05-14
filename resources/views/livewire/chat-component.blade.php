09ff3a0ba31b89e5278af967fe59491e063cb19a
Pedro Gonzalez <pedro.gch01@gmail.com>
2024-05-14 20:29:04 -0300

    actualizo las vistas

--- resources/views/livewire/chat-component.blade.php
+++ resources/views/livewire/chat-component.blade.php
@@ -1,17 +1,139 @@
 <div>
     {{-- Because she competes with no one, no one can compete with her. --}}
-    <h1>_Tus chats</h1>
-    <div>
-      <ul>
-        @foreach ($conversation as $convoItem )
-          <li>{{$convoItem['username']}} : {{$convoItem['message']}}</li>
-        @endforeach
-      </ul>
+    <div class="row h-100"> <!-- Cambiamos el tamaño a 100% -->
+      <!-- Conversaciones -->
+      <div class="col-md-4 mb-3 conversations-border"> <!-- Cambiamos el tamaño a 30% y aplicamos el borde derecho -->
+        <!-- Conversación 1 -->
+        <div class="d-flex align-items-center">
+          <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="Persona 1" class="rounded-image me-3">
+          <div>
+            <h5>Nombre Persona 1</h5>
+            <span class="mensaje-extracto">
+              <p class="mensaje-extracto-contenedor">
+                Último mensaje enviado por Persona 1
+              </p>
+            </span>
+          </div>
+        </div>
+        <!-- Conversación 2 -->
+        <div class="d-flex align-items-center mt-3">
+          <img src="https://randomuser.me/api/portraits/women/2.jpg" alt="Persona 2" class="rounded-image me-3">
+          <div>
+            <h5>Nombre Persona 2</h5>
+            <span class="mensaje-extracto">
+              <p class="mensaje-extracto-contenedor">
+                Último mensaje enviado por Persona 2
+              </p>
+            </span>
+          </div>
+        </div>        <div class="d-flex align-items-center">
+          <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="Persona 1" class="rounded-image me-3">
+          <div>
+            <h5>Nombre Persona 1</h5>
+            <span class="mensaje-extracto">
+              <p class="mensaje-extracto-contenedor">
+                Último mensaje enviado por Persona 1
+              </p>
+            </span>
+          </div>
+        </div>
+        <!-- Conversación 2 -->
+        <div class="d-flex align-items-center mt-3">
+          <img src="https://randomuser.me/api/portraits/women/2.jpg" alt="Persona 2" class="rounded-image me-3">
+          <div>
+            <h5>Nombre Persona 2</h5>
+            <span class="mensaje-extracto">
+              <p class="mensaje-extracto-contenedor">
+                Último mensaje enviado por Persona 2
+              </p>
+            </span>
+          </div>
+        </div>        <div class="d-flex align-items-center">
+          <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="Persona 1" class="rounded-image me-3">
+          <div>
+            <h5>Nombre Persona 1</h5>
+            <span class="mensaje-extracto">
+              <p class="mensaje-extracto-contenedor">
+                Último mensaje enviado por Persona 1
+              </p>
+            </span>
+          </div>
+        </div>
+        <!-- Conversación 2 -->
+        <div class="d-flex align-items-center mt-3">
+          <img src="https://randomuser.me/api/portraits/women/2.jpg" alt="Persona 2" class="rounded-image me-3">
+          <div>
+            <h5>Nombre Persona 2</h5>
+            <span class="mensaje-extracto">
+              <p class="mensaje-extracto-contenedor">
+                Último mensaje enviado por Persona 2
+              </p>
+            </span>
+          </div>
+        </div>        <div class="d-flex align-items-center">
+          <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="Persona 1" class="rounded-image me-3">
+          <div>
+            <h5>Nombre Persona 1</h5>
+            <span class="mensaje-extracto">
+              <p class="mensaje-extracto-contenedor">
+                Último mensaje enviado por Persona 1
+              </p>
+            </span>
+          </div>
+        </div>
+        <!-- Conversación 2 -->
+        <div class="d-flex align-items-center mt-3">
+          <img src="https://randomuser.me/api/portraits/women/2.jpg" alt="Persona 2" class="rounded-image me-3">
+          <div>
+            <h5>Nombre Persona 2</h5>
+            <span class="mensaje-extracto">
+              <p class="mensaje-extracto-contenedor">
+                Último mensaje enviado por Persona 2
+              </p>
+            </span>
+          </div>
+        </div>        <div class="d-flex align-items-center">
+          <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="Persona 1" class="rounded-image me-3">
+          <div>
+            <h5>Nombre Persona 1</h5>
+            <span class="mensaje-extracto">
+              <p class="mensaje-extracto-contenedor">
+                Último mensaje enviado por Persona 1
+              </p>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <!-- Bloque de Conversación -->
+      <div class="col-md-8 mb-3 position-relative"> <!-- Cambiamos el tamaño a 70% y establecemos posición relativa -->
+        <!-- Contenedor de mensajes -->
+        <div class="row">
+          <div class="conversation">
+            @foreach ($conversation as $convoItem )
+              <!-- Mensajes enviados emisor -->
+              @if (Auth::user()->id == $convoItem['user_id'])
+                <div class="message sent-message">
+                  {{$convoItem['message']}}
+                </div>
+              @else
+                <!-- Mensajes recibidos receptor -->
+                <div class="message received-message">
+                  {{$convoItem['message']}}
+                </div>
+              @endif
+            @endforeach
+          </div>
+
+          <!-- Campo de entrada y botón para enviar mensaje -->
+          <div class="input-group mt-3">
+            <form wire:submit="submitMessage">
+            <input wire:model="message" wire:key="{{now()}}" class="form-control" placeholder="Escribe tu mensaje...">
+            <button type="submit" class="btn btn-primary">Enviar</button>
+            </form>
+          </div>
+        </div>
+
+      </div>
     </div>
-
-
-    <form wire:submit="submitMessage">
-      <input wire:model="message" wire:key="{{now()}}"/>
-      <button type="submit">send</button>
-    </form>
 </div>
