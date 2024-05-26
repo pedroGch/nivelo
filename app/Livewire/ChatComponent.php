8398074d3193e33f9b82e8b1b181508a9b09abb8
Pedro Gonzalez <pedro.gch01@gmail.com>
2024-05-26 15:44:31 -0300

    agrego llamada a la vista chat y valido chat

--- app/Livewire/ChatComponent.php
+++ app/Livewire/ChatComponent.php
@@ -7,6 +7,8 @@ use Livewire\Attributes\On;
 use App\Events\MessageEvent;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Message;
+use App\Models\Chat;
+use Illuminate\Http\Request;
 
 class ChatComponent extends Component
 {
@@ -15,9 +17,11 @@ class ChatComponent extends Component
 
   public function mount()
   {
+    /*
+    aca debo buscar las conversaciones del usuario
 
+    */
     $messages = Message::all();
-
     foreach ($messages as $m)
     {
       $this->conversation[] = [
@@ -35,14 +39,48 @@ class ChatComponent extends Component
   #[On('echo:chat-channel,MessageEvent')]
   public function listenForMessage($data)
   {
-    //dd($data);
     $this->conversation[] = [
       'username' => $data['username'],
       'message'  => $data['message']
     ];
   }
+
+  public function chatInbox(Chat $chat)
+  {
+    //$this->authorize('view', $chat);
+    return view('chat.chat', compact('chat'));
+  }
+
   public function render()
   {
-      return view('livewire.chat-component');
+    return view('livewire.chat-component');
+  }
+
+  public function startChat(Request $request)
+  {
+
+    $request->validate([
+      'receiver_id' => 'required|exists:users,id',
+    ]);
+    $sender_id = Auth::id();
+    $receiver_id = $request->input('receiver_id');
+    // Verificar si ya existe un chat entre estos usuarios
+    $chat = Chat::where(function ($query) use ($sender_id, $receiver_id) {
+      $query->where('sender_id', $sender_id)
+            ->where('receiver_id', $receiver_id);
+      })->orWhere(function ($query) use ($sender_id, $receiver_id) {
+          $query->where('sender_id', $receiver_id)
+                ->where('receiver_id', $sender_id);
+      })->first();
+
+    if (!$chat) {
+      // Crear un nuevo chat si no existe
+      $chat = Chat::create([
+        'sender_id' => $sender_id,
+        'receiver_id' => $receiver_id,
+      ]);
+    }
+
+    return redirect()->route('chatInbox', ['chat' => $chat->id]);
   }
 }
