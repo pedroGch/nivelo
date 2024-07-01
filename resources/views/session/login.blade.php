934f81790a9b8497ce51ba062d85d4fce53496d0
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2024-07-01 00:08:50 -0300

    fix back() on login, ahora a 'home'

--- resources/views/session/login.blade.php
+++ resources/views/session/login.blade.php
@@ -9,7 +9,7 @@
   <div class="row d-flex vh-100 no_maring-right">
     <div class="col-12 col-md-6 col-lg-4 mt-lg-5 ms-lg-5 pt-3 container-xxl bg-white rounded shadow-sm">
       <div class="pt-5 d-flex justify-content-center my-6">
-        <a href="#" onclick="event.preventDefault(); window.history.back();"><img src="{{ url('/img/icons/back_icon.svg') }}" alt="atrás" class="mt-2 me-4" height="40px"></a>
+        <a href="{{ route('home') }}"><img src="{{ url('/img/icons/back_icon.svg') }}" alt="atrás" class="mt-2 me-4" height="40px"></a>
         <a href="{{ route('home') }}"><img src="{{ url('/img/logo_horizontal.png') }}" alt="logo de nivelo"></a>
       </div>
       <div class="row pb-5 mt-4 mx-auto border-top redondeo-superior-login shadow-sm-top">
