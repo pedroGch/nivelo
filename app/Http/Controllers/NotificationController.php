a2b61d39795673bd4df0734f303404365ab7c96b
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2024-07-03 01:21:40 -0300

    creo controlador NotificationController

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Notification;
+
+class NotificationController extends Controller
+{
+    /**
+     * Muestra las notificaciones del usuario autenticado
+     */
+    public function index()
+    {
+        $notifications = Notification::where('user_id', auth()->id())->get();
+        return view('notifications.index', compact('notifications'));
+    }
+
+    /**
+     * Marca una notificación como leída
+     * @param int $id
+     */
+    public function markAsRead($id)
+    {
+        $notification = Notification::find($id);
+        $notification->update(['read' => true]);
+        return redirect()->route('notifications.index');
+    }
+}
