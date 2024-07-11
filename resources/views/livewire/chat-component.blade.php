db1eab904cfa9244f237b427306b4507b7c81139
Pedro Gonzalez <pedro.gch01@gmail.com>
2024-07-11 14:10:22 -0300

    vista chat adaptada

--- resources/views/livewire/chat-component.blade.php
+++ resources/views/livewire/chat-component.blade.php
@@ -1,17 +1,16 @@
 <div>
   <div class="row h-100"> <!-- Cambiamos el tamaño a 100% -->
     <!-- Conversaciones -->
-    <div class="col-md-4 mb-3 conversations-border pe-0"> <!-- Cambiamos el tamaño a 30% y aplicamos el borde derecho -->
+    <div class="col-12 col-md-4 mb-3 conversations-border pe-0"> <!-- Cambiamos el tamaño a 100% en móvil y 30% en desktop y aplicamos el borde derecho -->
       <!-- Conversación 1 -->
       @foreach ($chats as $chat)
-        <a href="#" wire:click.prevent="selectChat({{ $chat->id }})" class="fw-bold text-reset text-decoration-none">
+        <a href="#" wire:click.prevent="selectChat({{ $chat->id }})" class="text-reset text-decoration-none">
           <div class="d-flex align-items-center {{ $selectedChatId === $chat->id ? 'selected-chat' : '' }}">
-            <img src="{{ url('/img/avatars/'.$chat->receiver->avatar) }}" alt="avatar" class="rounded-image me-3">
+            <img src="{{ url('/img/avatars/'.$chat->receiver->avatar) }}" alt="avatar" class="rounded-image me-3" style="width: 50px; height: 50px;">
             <div class="pt-3 ps-2">
-              <h2 class="h5">{{ $chat->sender_id == Auth::id() ? $chat->receiver->username : $chat->sender->name }}</h2>
+              <h2 class="h6 fw-bold">{{ $chat->sender_id == Auth::id() ? $chat->receiver->username : $chat->sender->name }}</h2> <!-- Cambié el tamaño del encabezado -->
               <span class="mensaje-extracto">
                 <p class="mensaje-extracto-contenedor">
-                  {{-- Último mensaje enviado por {{ $chat->sender_id == Auth::id() ? 'tú' : $chat->sender->name }} --}}
                   Iniciado el {{ $chat->created_at }}
                 </p>
               </span>
@@ -22,60 +21,60 @@
     </div>
 
     <!-- Bloque de Conversación -->
-    <div class="col-md-8 mb-3 position-relative"> <!-- Cambiamos el tamaño a 70% y establecemos posición relativa -->
+    <div class="col-12 col-md-8 mb-3 position-relative"> <!-- Cambiamos el tamaño a 100% en móvil y 70% en desktop y establecemos posición relativa -->
       <!-- Contenedor de mensajes -->
       <div class="row">
-        <div class="conversation shadow-sm">
-          @foreach ($conversation as $convoItem )
+        <div class="conversation shadow-sm" style="max-height: 70vh; overflow-y: auto;"> <!-- Ajustamos el contenedor de mensajes -->
+          @foreach ($conversation as $convoItem)
             <!-- Mensajes enviados emisor -->
             @if (isset($convoItem['user_id']) && Auth::user()->id == $convoItem['user_id'])
               <div class="message sent-message">
                 <p>{{$convoItem['message']}}</p>
-                {{-- <small class="text-muted">{{$convoItem['created_at']}}</small> --}}
               </div>
             @else
               <!-- Mensajes recibidos receptor -->
               <div class="message received-message">
                 <p>{{$convoItem['message']}}</p>
-                {{-- <small class="text-muted">{{$convoItem['created_at']}}</small> --}}
               </div>
             @endif
           @endforeach
         </div>
 
         <!-- Campo de entrada y botón para enviar mensaje -->
-        <form wire:submit.prevent="submitMessage"  class="input-group mt-3">
-            <input id="messageInput" wire:model="message" class="form-control" placeholder="Escribí tu mensaje...">
-            <input type="hidden" wire:model="chat_id">
-            <button id="sendButton" type="submit" class="btn bg-verde-principal btn-verde-hover text-light fw-bold">Enviar</button>
+        <form wire:submit.prevent="submitMessage" class="input-group mt-3">
+          <input id="messageInput" wire:model="message" class="form-control" placeholder="Escribí tu mensaje...">
+          <input type="hidden" wire:model="chat_id">
+          <button id="sendButton" type="submit" class="btn bg-verde-principal btn-verde-hover text-light fw-bold" disabled>Enviar</button>
         </form>
       </div>
     </div>
   </div>
+
   <script>
     Livewire.on('messageSent', () => {
         @this.call('loadMessages');
     });
-</script>
-<script>
-  document.addEventListener('DOMContentLoaded', function() {
-      const messageInput = document.getElementById('messageInput');
-      const sendButton = document.getElementById('sendButton');
+  </script>
 
-      messageInput.addEventListener('input', function() {
-          if (messageInput.value.trim() === '') {
-              sendButton.disabled = true;
-          } else {
-              sendButton.disabled = false;
-          }
-      });
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const messageInput = document.getElementById('messageInput');
+        const sendButton = document.getElementById('sendButton');
 
-      // Inicializar el estado del botón cuando se carga la página
-      if (messageInput.value.trim() === '') {
-          sendButton.disabled = true;
-      } else {
-          sendButton.disabled = false;
-      }
-  });
-</script>
+        messageInput.addEventListener('input', function() {
+            if (messageInput.value.trim() === '') {
+                sendButton.disabled = true;
+            } else {
+                sendButton.disabled = false;
+            }
+        });
+
+        // Inicializar el estado del botón cuando se carga la página
+        if (messageInput.value.trim() === '') {
+            sendButton.disabled = true;
+        } else {
+            sendButton.disabled = false;
+        }
+    });
+  </script>
 </div>
